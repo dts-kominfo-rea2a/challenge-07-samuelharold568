a2ce4357cd508d39c960ebc85501045267958519
samuelharold568 <samuelharold327@gmail.com>
2022-06-12 20:35:29 +0700

    memperbaiki bug1

--- promise.js
+++ promise.js
@@ -1,12 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = (emosi) => {
-  return new Promise((resolve, reject) => {
-    if(emosi) {
-      
-    }
-  })
+
+const promiseOutput = async (emosi) => {
+  let x = await promiseTheaterIXX();
+  let y = await promiseTheaterVGC();
+  let gabungan = x.concat(y);
+  let result = 0;
+  gabungan.map((x) => x.hasil === emosi && (result += 1));
+  return result;
 };
 
 module.exports = {
