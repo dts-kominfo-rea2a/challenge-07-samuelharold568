f030373edfc14d9b414d7107d1fbbee2ad0916c4
samuelharold568 <samuelharold327@gmail.com>
2022-06-12 17:15:59 +0700

    membuat fungsi promieOutput

--- promise.js
+++ promise.js
@@ -1,7 +1,13 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise((resolve, reject) => {
+    if(emosi) {
+      
+    }
+  })
+};
 
 module.exports = {
   promiseOutput,
